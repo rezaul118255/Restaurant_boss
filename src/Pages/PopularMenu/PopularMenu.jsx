def4be3ac4eed587bcb0db742df8e0884886d5ae
rezaul118255 <rezaul118255@gmail.com>
2023-05-30 17:33:57 +0600

    Menu Category Add

--- src/Pages/PopularMenu/PopularMenu.jsx
+++ src/Pages/PopularMenu/PopularMenu.jsx
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../Components/SectionTitle';
 import MenuItem from '../../Shared/MenuItem';
+import UseMenu from '../../Hooks/UseMenu';
 
 const PopularMenu = () => {
-    const [Menu, setMenu] = useState([])
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data.filter(item => item.category === "popular");
-                setMenu(popularItems)
+    const [Menu] = UseMenu()
+    const popular = Menu.filter(item => item.category === "popular")
+    // const [Menu, setMenu] = useState([])
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             const popularItems = data.filter(item => item.category === "popular");
+    //             setMenu(popularItems)
 
 
-            })
+    //         })
 
-    }, [])
+    // }, [])
 
     return (
         <div className='mb-12'>
@@ -26,7 +29,7 @@ const PopularMenu = () => {
             </SectionTitle>
             <div className='grid   md:grid-cols-2 gap-6'>
                 {
-                    Menu.map(item => <MenuItem
+                    popular.map(item => <MenuItem
                         key={item._id}
                         item={item}
                     ></MenuItem>)
